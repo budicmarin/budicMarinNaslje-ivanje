03457dbccb79a824395ff85deb84c6e556f27606
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-03-24 11:46:46 +0100

    Add project files.

---
+++ Osoba.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinNasljeđivanje
+{
+    class Osoba
+    {
+        string ime;
+        string prezime;
+
+        public string Prezime { get => prezime; set => prezime = value; }
+        public string Ime { get => ime; set => ime = value; }
+        public override string ToString()
+        {
+            string ispis = "Ime " + this.ime + " Prezime: " + this.prezime;
+            return ispis;
+        }
+        public string DohvatiPodatke()
+        {
+            string ispis = "\nIme " + this.ime + " \nPrezime: " + this.prezime;
+            return ispis;
+        }
+    }
+}
---
+++ Ucenik.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace budicMarinNasljeđivanje
+{
+    class Ucenik : Osoba
+    {
+        private int razred;
+
+        public int Razred { get => razred; set => razred = value; }
+    }
+}
